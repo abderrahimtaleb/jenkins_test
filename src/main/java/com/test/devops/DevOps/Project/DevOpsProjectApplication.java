0c3d4ac4816ab1b7068205c03d8f48b6d1be15a8
Abderrahim Taleb <abderrahim.taleb@neoxia.com>
2019-03-11 11:56:08 +0100

    fist one

---
+++ src/main/java/com/test/devops/DevOps/Project/DevOpsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.devops.DevOps.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjectApplication.class, args);
+	}
+
+}
